e0339d62c77a46ecd03a697eb62b832edde031c5
vovabatsyk <vovabatsyk@gmail.com>
2023-01-28 15:57:48 +0200

    init routes

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+interface IHomeProps {}
+
+export const Home: FC<IHomeProps> = () => {
+  return <div>Home</div>;
+};
---
+++ src/pages/Search.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+interface ISearchProps {}
+
+export const Search: FC<ISearchProps> = () => {
+  return <div>Search</div>;
+};
