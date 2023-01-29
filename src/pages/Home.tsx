fb12d507a3e2494ebb2ec02f21e6ec1695713150
vovabatsyk <vovabatsyk@gmail.com>
2023-01-29 14:55:41 +0200

    init redux

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,24 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Navbar } from '../components/Navbar';
 import { Sidebar } from '../components/Sidebar';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { getHomePageVideos } from '../store/reducers/getHomePageVideos';
 
 interface IHomeProps {}
 
 export const Home: FC<IHomeProps> = () => {
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+
+  useEffect(() => {
+    dispatch(getHomePageVideos(false));
+    console.log(videos);
+  }, [dispatch]);
+
+  const showVideo = () => {
+    console.log(videos);
+  };
+
   return (
     <div className='max-h-screen overflow-hidden bg-zinc-900'>
       <div style={{ height: '7.5vh' }}>
@@ -15,6 +29,7 @@ export const Home: FC<IHomeProps> = () => {
         style={{ height: '92.5vh' }}
       >
         <Sidebar />
+        <button onClick={showVideo}>video</button>
       </div>
     </div>
   );
