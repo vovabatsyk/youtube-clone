39532d6693bf3326a90a0b9373eed9579fbc1313
vovabatsyk <vovabatsyk@gmail.com>
2023-02-03 16:56:17 +0200

    search videos

---
+++ src/store/reducers/getSearchPageVideos.ts
@@ -0,0 +1,30 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { RootState } from '..';
+import { parseData } from '../../utils';
+import { YOUTUBE_API_URL } from '../../utils/constants';
+
+const API_KEY = import.meta.env.VITE_REACT_APP_YOUTUBE_DATA_API_KEY;
+
+export const getSearchPageVideos = createAsyncThunk(
+  'youtubeApp/searchPageVideos',
+  async (isNext: boolean, { getState }) => {
+    const {
+      youtubeApp: { nextPageToken: nextPageTokenFromState, videos, searchTerm },
+    } = getState() as RootState;
+
+    const {
+      data: { items, nextPageToken },
+    } = await axios.get(
+      `${YOUTUBE_API_URL}/search?q=${searchTerm}&key=${API_KEY}&part=snippet&type=video&${
+        isNext ? `pageToken=${nextPageTokenFromState}` : ''
+      }`
+    );
+    const parsedData = await parseData(items);
+    if (parsedData) {
+      const data = { parsedData: [...videos, ...parsedData], nextPageToken };
+      return data;
+    }
+    return [];
+  }
+);
